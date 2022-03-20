d8c2ee55da8d43017e587d70187ac7694708582f
Ryan Cummins <brokenrhino@gmail.com>
2022-03-20 00:19:29 -0400

    Created Rewind Project

---
+++ Rewind/Rewind/RewindApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewindApp.swift
+//  Rewind
+//
+//  Created by Ryan Cummins on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RewindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
